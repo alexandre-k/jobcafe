bf325f59a5e8ffcc01f4734abd199cf0ed5fc558
Alexandre Krispin <alexandre.krispin@upstairs.co.jp>
2019-09-02 07:50:50 +0900

    Add function to draw a rectangle with ellipsis

--- ui/Utils.js
+++ ui/Utils.js
@@ -53,3 +53,17 @@ const formatDate = aDate => {
     const createdDate = new Date(aDate);
     return `${createdDate.getUTCFullYear()} ${monthNames[createdDate.getMonth()]} ${createdDate.getUTCDate()}`;
 }
+
+function drawRec(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height);
+
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y);
+
+    ctx.closePath();
+}
